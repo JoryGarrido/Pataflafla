d61e339bad7fdeb79becb762bb86d05f3c1acd93
Jory Blaze Garrido <jorygarrido@gmail.com>
2016-09-05 11:51:21 -0600

    created ng-click on videolist to go to video page

--- public/components/videoList/videoListController.js
+++ public/components/videoList/videoListController.js
@@ -1,12 +1,15 @@
 app.controller('videoListController', function($scope,$http){
   $scope.view = {};
+  $scope.goToVid = function(id){
+    var arr = id.split('=');
+    location.assign('/#/video/' + arr[1]);
+  }
 
   $http({
     method: 'GET',
     url: '/videolist'
   })
   .then(function(data){
-    console.log(data);
     $scope.view.videos = data.data;
   });
 });
