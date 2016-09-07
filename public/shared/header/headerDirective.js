7c279b1bd612693e8f6cbbbdef5bb6b49a21e82e
Jory Blaze Garrido <jorygarrido@gmail.com>
2016-09-06 20:22:04 -0600

    added semi colons

--- public/shared/header/headerDirective.js
+++ public/shared/header/headerDirective.js
@@ -2,17 +2,17 @@ app.directive('pffHeader', function(){
   var headerController = function($scope){
     $scope.home = function(){
       location.assign('/');
-    }
+    };
     $scope.postPage = function(){
       location.assign('/videopost');
-    }
+    };
     $scope.videoList = function(){
       location.assign('/videolist');
-    }
-  }
+    };
+  };
   return{
     templateUrl:
     'shared/header/headerView.html',
     controller: headerController
-  }
+  };
 });
